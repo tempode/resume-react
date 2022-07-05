b7b079d04d36d4dcdbf1e0e1bf13c86bc1148497
udaybegyall <udaybegyall12345@gmail.com>
2022-07-05 14:33:16 +0530

    changed src

--- src/components/downloadbutton.tsx
+++ src/components/downloadbutton.tsx
@@ -2,7 +2,7 @@ import DownlaodIcon from '../assets/icons/downlaod.svg';
 
 const DownloadButton = () => {
     return (
-        <a title="Download CV" className="download-button" href="src/assets/resume.pdf" download>
+        <a title="Download CV" className="download-button" href="/src/assets/resume.pdf" download>
             <img src={DownlaodIcon} alt="Download Resume" />
         </a>
     );
