5124371680e22bca71f0410adb67a179f0e7b893
udaybegyall <udaybegyall12345@gmail.com>
2022-08-31 23:03:26 +0530

    added date to cc

--- src/utils/coCurriculars.ts
+++ src/utils/coCurriculars.ts
@@ -9,8 +9,8 @@ export const coCurriculars = [
         title: "Competative Programming",
         description: [
             "Participated in Hackerrank, hackerearth and Codeforces events.",
-            "Top 50 more than twice at codechef.",
-            "Four star at codechef."
+            "Top 50 more than twice at codechef. (2020, 2021)",
+            "Four star at codechef. (2020)"
         ]
     },
 ]
