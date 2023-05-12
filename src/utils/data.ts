212095c6bac8734d1a322a8b4102d01b4b468e7f
tempode <133360991+tempode@users.noreply.github.com>
2023-05-12 16:59:11 +0530

    Update data.ts
    Added the recent Internship.

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,5 +1,18 @@
 export const exprience = [
     {
+        title: "Software Engineering Intern",
+        company: " @ Capgimini",
+        date: "Feb 2023 - May 2023",
+        discription: [
+            "Developed expertise in C#, ASP.NET MVC, and ASP.NET Core while working on various projects as a software engineering intern.",
+            "Created a movie booking API using ASP.NET Core for a client, which involved designing the backend and developing the API's endpoints using best coding practices.",
+            "Utilized Angular to create an intuitive and responsive user interface for the movie booking web application.",
+            "Collaborated with a team of developers using Git and GitHub to manage code versioning and to ensure that all work was properly integrated.",
+            "Assisted in conducting code reviews to identify areas of improvement and optimize code performance.",
+            "Acquired experience in troubleshooting and debugging code using Visual Studio and various other debugging tools."
+        ]
+    }
+    {
         title: "Rust Intern",
         company: " @ Kootlabs",
         date: "April 2022 - June 2022",
